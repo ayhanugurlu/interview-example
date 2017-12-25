acc839d5a289face44d8d1156b30d3ac4f649165
root <root@AYHANUSLC.logo.com.tr>
2017-12-25 13:42:56 +0300

    initial commit

---
+++ src/main/java/com/au/example/interviewexample/InterviewExampleApplication.java
@@ -0,0 +1,12 @@
+package com.au.example.interviewexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewExampleApplication.class, args);
+	}
+}
